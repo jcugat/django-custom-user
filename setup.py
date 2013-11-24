ddf7b669628be32ab5d3f5aaa7b96c198cb8d0ce
Josep Cugat <jcugat@gmail.com>
2013-11-24 21:23:36 +0100

    Version 0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-custom-user',
-    version='0.1',
+    version='0.2',
     packages=['custom_user'],
     include_package_data=True,
     license='BSD License',
