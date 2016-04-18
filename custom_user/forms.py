23877139c860f243cf83ffd8a0db127a0695c650
Igor Gai <igor.gai@i.ua>
2016-04-18 13:50:53 +0300

    using standard django UserChangeForm (from django.contrib.auth.forms) to fix broken "change password" link bug

--- custom_user/forms.py
+++ custom_user/forms.py
@@ -1,7 +1,7 @@
 """EmailUser forms."""
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth.forms import UserChangeForm
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -80,39 +80,6 @@ class EmailUserCreationForm(forms.ModelForm):
         return user
 
 
-class EmailUserChangeForm(forms.ModelForm):
-
-    """A form for updating users.
-
-    Includes all the fields on the user, but replaces the password field
-    with admin's password hash display field.
-
-    """
-
-    password = ReadOnlyPasswordHashField(label=_("Password"), help_text=_(
-        "Raw passwords are not stored, so there is no way to see "
-        "this user's password, but you can change the password "
-        "using <a href=\"password/\">this form</a>."))
-
-    class Meta:
+class EmailUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
         model = get_user_model()
-        exclude = ()
-
-    def __init__(self, *args, **kwargs):
-        """Init the form."""
-        super(EmailUserChangeForm, self).__init__(*args, **kwargs)
-        f = self.fields.get('user_permissions', None)
-        if f is not None:
-            f.queryset = f.queryset.select_related('content_type')
-
-    def clean_password(self):
-        """Clean password.
-
-        Regardless of what the user provides, return the initial value.
-        This is done here, rather than on the field, because the
-        field does not have access to the initial value.
-
-        :return str password:
-
-        """
-        return self.initial["password"]
